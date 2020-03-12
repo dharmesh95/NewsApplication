4159c01f1d4569b8e722f8fbbda80096f58b1f77
Ankur Pravin Patel <n01324338@humber.ca>
2020-03-12 17:26:56 -0400

    added politics section

--- app/src/main/java/com/example/newsapp/ui/politics/PoliticsViewModel.java
+++ app/src/main/java/com/example/newsapp/ui/politics/PoliticsViewModel.java
@@ -1,16 +1,16 @@
-package com.example.newsapp.ui.slideshow;
+package com.example.newsapp.ui.politics;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class SlideshowViewModel extends ViewModel {
+public class PoliticsViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public SlideshowViewModel() {
+    public PoliticsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is slideshow fragment");
+        mText.setValue("This is Politics fragment");
     }
 
     public LiveData<String> getText() {
